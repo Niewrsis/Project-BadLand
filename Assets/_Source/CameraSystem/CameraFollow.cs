1d59fd88f5275df53142502d86801cbeabb33800
Niewrsis <154369161+Niewrsis@users.noreply.github.com>
2024-11-05 14:36:36 +0300

    twaek: new camera attribute camera

--- Assets/_Source/CameraSystem/CameraFollow.cs
+++ Assets/_Source/CameraSystem/CameraFollow.cs
@@ -8,11 +8,11 @@ namespace CameraSystem
         [SerializeField] private Transform target;
 
         [Header("Attributes")]
-        [SerializeField] private float followSpeed;
+        [SerializeField] private float followSpeed, yPosition;
 
         private void Update()
         {
-            transform.position = Vector3.Lerp(new Vector3(transform.position.x, 0, -20), new Vector2(target.position.x, 0), followSpeed * Time.deltaTime);
+            transform.position = Vector3.Lerp(new Vector3(transform.position.x, yPosition, -20), new Vector2(target.position.x, yPosition), followSpeed * Time.deltaTime);
         }
     }
 }
