51f02a35453c2b6717939480b18521c2b9bb9466
Niewrsis <154369161+Niewrsis@users.noreply.github.com>
2024-11-05 14:00:38 +0300

    tweak: Player
    = changed Player.cs
    + added config to player

--- Assets/_Source/PlayerSystem/Player.cs
+++ Assets/_Source/PlayerSystem/Player.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 namespace PlayerSystem
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class Player : MonoBehaviour
     {
-        //TODO: Config for movement speed & jump force
+        [Header("Attributes")]
+        public float MoveSpeed;
+        public float JumpForce;
+        public float RotationSpeed;
+
+        private Rigidbody2D _rb;
+        private Transform _transform;
+        public Transform Transform => _transform;
+        public Rigidbody2D Rb => _rb;
+
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _transform = GetComponent<Transform>();
+        }
     }
 }
